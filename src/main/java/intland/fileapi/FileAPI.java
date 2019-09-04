bf41950a11ae2d23086e95b728bed791d2fd3735
gergoszucs <gergobenceszucs@gmail.com>
2019-09-04 21:17:23 +0200

    Initial commit, sample project with gradle and spring

---
+++ src/main/java/intland/fileapi/FileAPI.java
@@ -0,0 +1,12 @@
+package main.java.intland.fileapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileAPI {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileAPI.class, args);
+    }
+}
