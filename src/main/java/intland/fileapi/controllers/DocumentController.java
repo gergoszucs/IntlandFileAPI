03c021c230fd9712200dd61a960f5f3935d8eea1
gergoszucs <gergobenceszucs@gmail.com>
2019-09-06 13:15:54 +0200

    Add CORS policy for localhost:4200

--- src/main/java/intland/fileapi/controllers/DocumentController.java
+++ src/main/java/intland/fileapi/controllers/DocumentController.java
@@ -1,8 +1,9 @@
-package main.java.intland.fileapi.controllers;
+package intland.fileapi.controllers;
 
-import main.java.intland.fileapi.interfaces.IDocumentService;
-import main.java.intland.fileapi.models.DocumentBatch;
+import intland.fileapi.interfaces.IDocumentService;
+import intland.fileapi.models.DocumentBatch;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ class DocumentController {
 	@Autowired
     private IDocumentService documentService;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/document")
     public DocumentBatch getDocumentBatch(@RequestParam(value="skip", required = true) int skip, @RequestParam(value="take", required = true) int take) {
         return documentService.getDocumentBatch(skip, take);
